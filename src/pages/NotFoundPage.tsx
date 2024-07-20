e5845a7f953d82d68d0e730feb12aea300c3708e
Dan Krutoholov <black3phoenix@gmail.com>
2024-07-20 14:44:29 +0200

    feat: update not found page and global css styles

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -8,9 +8,8 @@ export function NotFoundPage() {
     };
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Details Page</h1>
-            <p>Here are the details...</p>
+        <div className="p-4 w-full h-full flex flex-col justify-center items-center">
+            <h1>The page you were looking for does not exist.</h1>
             <button 
                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded" 
                 onClick={goToMainPage}
